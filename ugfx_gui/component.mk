46e10e30603a0c15f285620eb1cb0dcb085a5c5a
InfiniteYuan <infiniteyuan@outlook.com>
2018-08-13 20:08:00 +0800

    update component.mk

--- ugfx_gui/component.mk
+++ ugfx_gui/component.mk
@@ -3,10 +3,6 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-ifndef CONFIG_UGFX_GUI_ENABLE
-COMPONENT_ADD_INCLUDEDIRS := 
-COMPONENT_SRCDIRS :=
-else
 UGFXLIB = ugfx
 
 COMPONENT_ADD_INCLUDEDIRS := .\
@@ -18,11 +14,3 @@ COMPONENT_PRIV_INCLUDEDIRS +=  .\
     $(UGFXLIB) \
     $(UGFXLIB)/src \
 
-ifdef CONFIG_UGFX_USE_CUSTOM_DRIVER
-COMPONENT_DEPENDS += $(call dequote,$(CONFIG_UGFX_CUSTOM_DRIVER_COMPONENT_NAME))
-else
-COMPONENT_DEPENDS += ugfx_driver
-endif
-
-COMPONENT_EXTRA_INCLUDES += $(PROJECT_PATH)/$(call dequote,$(CONFIG_UGFX_PROJ_RESOURCE_PATH))  
-endif  #CONFIG_UGFX_GUI_ENABLE
